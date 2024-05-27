41d19bb3d3420c13d9c27488e134966c1e0a1e2c
SteelParrot <martinpop06@gmail.com>
2024-05-27 14:35:34 +0200

    jar

--- src/game/componenets/MainMenu.java
+++ src/game/componenets/MainMenu.java
@@ -2,8 +2,11 @@ package game.componenets;
 
 import game.GameState;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 /**
  * Main menu
@@ -22,7 +25,7 @@ public class MainMenu extends JPanel {
         setLayout(null);
 
         JButton button = new JButton();
-        button.setBounds(100,350,700,100);
+        button.setBounds(100,450,700,100);
         button.setHorizontalAlignment(SwingConstants.CENTER);
         button.setText("CLICK HERE TO START THE GAME");
         button.setForeground(new Color(119, 176, 170));
@@ -32,6 +35,20 @@ public class MainMenu extends JPanel {
         button.setFont(new Font("Ariel", Font.PLAIN, 25));
         button.addActionListener(e -> gameStateManager.setCurrentState(GameState.GAME_MODE_CHOICE));
 
+        JLabel logo = new JLabel();
+        logo.setBounds(100,150,700,200);
+        logo.setBackground(new Color(0, 60, 67));
+        logo.setOpaque(true);
+
+        try {
+            BufferedImage img = ImageIO.read(getClass().getResourceAsStream("/frame/logo.png"));
+            Image img2 = img.getScaledInstance(logo.getWidth(), logo.getHeight(),Image.SCALE_SMOOTH);
+            logo.setIcon(new ImageIcon(img2));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        add(logo);
         add(button);
         setVisible(false);
     }
